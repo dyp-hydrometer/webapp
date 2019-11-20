833377037f12963f5ab286945cd6d0a997bcf5fc
Evan Campbell <evangarreth@gmail.com>
2019-11-20 16:32:13 -0600

    ignore this im just lazy

--- backend/fakedata.py
+++ backend/fakedata.py
@@ -9,17 +9,29 @@ fake = Faker()
 now = datetime.datetime.utcnow()
 headers = {'Content-type': 'application/json'}
 
-for x in range(0, 100):
-    data = {}
-    data["temp"] = fake.pyfloat(min_value=75., max_value=79.)
+for x in range(0,6):
+    profile = {}
+    profile["name"] = f"profile #{x}"
+    profile["description"] = f"{x}"
+    profile["req_temp"] = fake.pyfloat(min_value=75., max_value=79.)
     temp = fake.pyfloat(min_value=1, max_value=2)
     while temp > 1.050 and temp < 1.010:
         temp = float("{0:.3f}".format(fake.pyfloat(min_value=1, max_value=2)))
-    data["specific_gravity"] = temp
-    # add x * 5 seconds to the time of first execution
-    b = now + datetime.timedelta(0,x*5)
-    data["time"] = b.strftime("%Y-%m-%d %H:%M:%S")
-    data["rssi"] = fake.pyint(min_value=-70, max_value=-30, step=1)
-    print(data)
-    resp = requests.put('http://localhost:5000/api/hydrometers/2/reading/', data=json.dumps(data), headers=headers)
-    print(resp.status_code)
+    profile["req_gravity"] = temp
+    profile["duration"] = str(datetime.timedelta(days=14))
+    resp = requests.post(f"http://localhost:5000/api/profiles/", data=json.dumps(profile), headers=headers)
+    #resp = requests.get('http://localhost:5000/api/hydrometers/'+x, headers=headers)
+    #if resp.status_code == 201:
+    '''for y in range(0, 20):
+        data = {}
+        data["temp"] = fake.pyfloat(min_value=75., max_value=79.)
+        temp = fake.pyfloat(min_value=1, max_value=2)
+        while temp > 1.050 and temp < 1.010:
+            temp = float("{0:.3f}".format(fake.pyfloat(min_value=1, max_value=2)))
+        data["specific_gravity"] = temp
+        # add x * 5 seconds to the time of first execution
+        b = now + datetime.timedelta(0,y*5)
+        data["time"] = b.strftime("%Y-%m-%d %H:%M:%S")
+        print(data)
+        resp = requests.put(f'http://localhost:5000/api/hydrometers/{x}/reading/', data=json.dumps(data), headers=headers)
+        print(resp.status_code)'''
